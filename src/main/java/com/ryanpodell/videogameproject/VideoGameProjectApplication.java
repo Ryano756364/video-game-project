8ab2a3172ca1030b80b246de9b2347208a3eadb1
rodell <ryanodell2668@gmail.com>
2023-03-12 19:25:10 -0400

    Project setup

---
+++ src/main/java/com/ryanpodell/videogameproject/VideoGameProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ryanpodell.videogameproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanpodell/videogameproject/VideoGameProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanpodell.videogameproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
