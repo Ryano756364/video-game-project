a3228086f2708970b8622af2ab3b8eab14f690d4
rodell <ryanodell2668@gmail.com>
2023-03-12 19:49:09 -0400

    Loose coupling

--- src/main/java/com/ryanpodell/videogameproject/AppGamingBasicJava.java
+++ src/main/java/com/ryanpodell/videogameproject/AppGamingBasicJava.java
@@ -2,12 +2,22 @@ package com.ryanpodell.videogameproject;
 
 import com.ryanpodell.videogameproject.game.GameRunner;
 import com.ryanpodell.videogameproject.game.MarioGame;
+import com.ryanpodell.videogameproject.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 
-        var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+        //var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);  //this will not work because GameRunner class is tightly coupled to MarioGame
         gameRunner.run();
     }
 }
+
+// A car engine is tightly coupled - changing an engine takes a lot of work
+// A wheel is loosely coupled to a car
+// Laptop is not tightly coupled to any specific place
+// However, a desktop computer is
+
+// Coupling is very important in building great software
+// Want loose coupling as much as possible
--- src/main/java/com/ryanpodell/videogameproject/game/GameRunner.java
+++ src/main/java/com/ryanpodell/videogameproject/game/GameRunner.java
@@ -1,9 +1,9 @@
 package com.ryanpodell.videogameproject.game;
 
 public class GameRunner {
-        MarioGame game;
+        private SuperContraGame game;
 
-        public GameRunner(MarioGame game){
+        public GameRunner(SuperContraGame game){
             this.game = game;
         }
 
