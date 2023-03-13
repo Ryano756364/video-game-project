69b1362ad1e545da27f7e7d50ce05bc4365a6f26
rodell <ryanodell2668@gmail.com>
2023-03-13 09:53:45 -0400

    Added PacmanGame class

--- src/main/java/com/ryanpodell/videogameproject/AppGamingBasicJava.java
+++ src/main/java/com/ryanpodell/videogameproject/AppGamingBasicJava.java
@@ -2,13 +2,15 @@ package com.ryanpodell.videogameproject;
 
 import com.ryanpodell.videogameproject.game.GameRunner;
 import com.ryanpodell.videogameproject.game.MarioGame;
+import com.ryanpodell.videogameproject.game.PacmanGame;
 import com.ryanpodell.videogameproject.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 
-        var game = new MarioGame();
+        //var game = new MarioGame();
         //var game = new SuperContraGame();
+        var game = new PacmanGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
